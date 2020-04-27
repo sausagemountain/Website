f6ea98dcf516da07a3e877079f682010454b58d6
Mikhail <miha.vanya@gmail.com>
2020-04-27 11:00:36 +0500

    update template
    Template updated in accordance with [Template.xaml](https://github.com/Wox-launcher/Wox/blob/master/Wox/Themes/ThemeBuilder/Template.xaml)

--- client/app/scripts/controllers/theme.builder.js
+++ client/app/scripts/controllers/theme.builder.js
@@ -10,38 +10,54 @@
 angular.module('woxApp')
     .controller('ThemeBuilderCtrl', function ($scope) {
 
-        var template = '<ResourceDictionary  xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"> \
-    <ResourceDictionary.MergedDictionaries> \
-        <ResourceDictionary Source="Base.xaml"></ResourceDictionary> \
-    </ResourceDictionary.MergedDictionaries> \
-    <Style x:Key="QueryBoxStyle" BasedOn="{StaticResource BaseQueryBoxStyle}" TargetType="{x:Type TextBox}"> \
-        <Setter Property="Background" Value="<queryBoxBackgroundColor>"/> \
-        <Setter Property="Foreground" Value="<queryBoxTextColor>" /> \
-    </Style> \
-    <Style x:Key="WindowBorderStyle" BasedOn="{StaticResource BaseWindowBorderStyle}" TargetType="{x:Type Border}"> \
-        <Setter Property="Background" Value="<backgroundColor>"></Setter> \
-    </Style> \
-    <Style x:Key="WindowStyle" TargetType="{x:Type Window}" BasedOn="{StaticResource BaseWindowStyle}" > \
-    </Style> \
-    <Style x:Key="PendingLineStyle" BasedOn="{StaticResource BasePendingLineStyle}" TargetType="{x:Type Line}" > \
-    </Style> \
-    <Style x:Key="ItemTitleStyle" BasedOn="{StaticResource BaseItemTitleStyle}" TargetType="{x:Type TextBlock}" > \
-        <Setter Property="Foreground" Value="<resultTitleColor>"></Setter> \
-    </Style> \
-    <Style x:Key="ItemSubTitleStyle" BasedOn="{StaticResource BaseItemSubTitleStyle}" TargetType="{x:Type TextBlock}" > \
-        <Setter Property="Foreground"  Value="<resultSubTitleColor>"></Setter> \
-    </Style> \
-    <Style x:Key="ItemTitleSelectedStyle" BasedOn="{StaticResource BaseItemTitleSelectedStyle}" TargetType="{x:Type TextBlock}"> \
-        <Setter Property="Foreground" Value="<resultSelectedTitleColor>" /> \
-    </Style> \
-    <Style x:Key="ItemSubTitleSelectedStyle" BasedOn="{StaticResource BaseItemSubTitleSelectedStyle}" TargetType="{x:Type TextBlock}"> \
-        <Setter Property="Foreground" Value="<resultSelectedSubTitleColor>" /> \
-    </Style> \
-    <SolidColorBrush x:Key="ItemSelectedBackgroundColor"><resultSelectedBackgroundColor></SolidColorBrush> \
-    <Style x:Key="ThumbStyle" BasedOn="{StaticResource BaseThumbStyle}" TargetType="{x:Type Thumb}"> \
-    </Style> \
-    <Style x:Key="ScrollBarStyle" BasedOn="{StaticResource BaseScrollBarStyle}" TargetType="{x:Type ScrollBar}"> \
-    </Style> \
+        var template = '<ResourceDictionary  xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"\
+             xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml">\
+    <ResourceDictionary.MergedDictionaries>\
+        <ResourceDictionary Source="Base.xaml"/>\
+    </ResourceDictionary.MergedDictionaries>\
+    <Style x:Key="QueryBoxStyle" BasedOn="{StaticResource BaseQueryBoxStyle}" TargetType="{x:Type TextBox}">\
+        <Setter Property="Foreground" Value="{%searchFieldTextColor%}" />\
+    </Style>\
+        <Style x:Key="QueryTextSuggestionBoxStyle" BasedOn="{StaticResource BaseQueryTextSuggestionBoxStyle}" TargetType="{x:Type TextBox}">\
+        <Setter Property="Background" Value="{%searchFieldBackgroundColor%}"/>\
+    </Style>\
+    <Style x:Key="WindowBorderStyle" BasedOn="{StaticResource BaseWindowBorderStyle}" TargetType="{x:Type Border}">\
+        <Setter Property="Background" Value="{%backgroundColor%}"/>\
+        <Setter Property="CornerRadius" Value="8" />\
+        <Setter Property="BorderBrush" Value="{%borderColor%}" />\
+        <Setter Property="BorderThickness" Value="10" />\
+    </Style>\
+    <Style x:Key="WindowStyle" TargetType="{x:Type Window}" BasedOn="{StaticResource BaseWindowStyle}" >\
+        <Setter Property="Width" Value="520"/>\
+    </Style>\
+    <Style x:Key="PendingLineStyle" BasedOn="{StaticResource BasePendingLineStyle}" TargetType="{x:Type Line}" >\
+    </Style>\
+    <!-- Item Style -->\
+    <Style x:Key="ItemTitleStyle" BasedOn="{StaticResource BaseItemTitleStyle}" TargetType="{x:Type TextBlock}" >\
+        <Setter Property="Foreground" Value="{%resultTextColor%}"/>\
+    </Style>\
+    <Style x:Key="ItemSubTitleStyle" BasedOn="{StaticResource BaseItemSubTitleStyle}" TargetType="{x:Type TextBlock}" >\
+        <Setter Property="Foreground"  Value="{%resultSubtextColor%}"/>\
+    </Style>\
+    <Style x:Key="ItemTitleSelectedStyle" BasedOn="{StaticResource BaseItemTitleSelectedStyle}" TargetType="{x:Type TextBlock}">\
+        <Setter Property="Foreground" Value="{%selectedResultForeground%}" />\
+    </Style>\
+    <Style x:Key="ItemSubTitleSelectedStyle" BasedOn="{StaticResource BaseItemSubTitleSelectedStyle}" TargetType="{x:Type TextBlock}">\
+        <Setter Property="Foreground" Value="{%selectedSubtextForeground%}" />\
+    </Style>\
+    <Color x:Key="ItemSelectedBackgroundColor">{%selectedResultBackgroundColor%}</Color>\
+    <!-- button style in the middle of the scrollbar -->\
+    <Style x:Key="ThumbStyle" BasedOn="{StaticResource BaseThumbStyle}" TargetType="{x:Type Thumb}">\
+        <Setter Property="Template">\
+            <Setter.Value>\
+                <ControlTemplate TargetType="{x:Type Thumb}">\
+                    <Border CornerRadius="2" DockPanel.Dock="Right" Background="{%scrollbarColor%}" BorderBrush="Transparent" BorderThickness="0" />\
+                </ControlTemplate>\
+            </Setter.Value>\
+        </Setter>\
+    </Style>\
+    <Style x:Key="ScrollBarStyle" BasedOn="{StaticResource BaseScrollBarStyle}" TargetType="{x:Type ScrollBar}">\
+    </Style>\
 </ResourceDictionary>';
 
         $scope.defaultTheme = $scope.theme = {
